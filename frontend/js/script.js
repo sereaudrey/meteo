f35fc1c9b5b941d3c7d569256a0777571ae35a6e
Deb <deborahdev.io@gmail.com>
2022-03-21 12:09:43 +0100

    clean code

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,56 +1,8 @@
 // api meteo
-const CLEFAPI = "e7898f9bfac6cb1ae2b74dd328bbb92c";
-
-let resultatsAPI;
-
-const actualWeather = document.querySelector(".actual_weather");
 const temperatureLocal = document.querySelector(".temperatureLocal");
-// const imgIcone = document.querySelector(".logo-meteo");
 const humidity = document.querySelector(".humidity");
 
-//géolocalisation du navigateur pour avoir le temps et l'heure
-// if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(
-//     (position) => {
-//       let long = position.coords.longitude;
-//       let lat = position.coords.latitude;
-//       AppelAPI(long, lat);
-//     },
-//     () => {
-//       alert(
-//         "Vous avez refusé la géolacalisation, l'application ne peut pas fonctionner, veuillez l'activer",
-//       );
-//     },
-//   );
-// }
-
-function AppelAPI(long, lat) {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${CLEFAPI}`,
-  )
-    .then((reponse) => {
-      return reponse.json();
-    })
-
-    .then((data) => {
-      // console.log(data);
-
-      resultatsAPI = data;
-
-      // actualWeather.innerText = resultatsAPI.current.weather[0].description;
-
-      // let actualHour = new Date().getHours();
-      // // icones dynamiques
-      // if (actualHour >= 6 && actualHour < 22) {
-      //   imgIcone.src = `styles/assets/jour/${resultatsAPI.current.weather[0].icon}.svg`;
-      // } else {
-      //   imgIcone.src = `styles/assets/nuit/${resultatsAPI.current.weather[0].icon}.svg`;
-      // }
-    });
-}
-
 const sensor = "1";
-
 const url = "http://192.168.90.251/assets/api-station/v2/lire.php";
 
 let request = new XMLHttpRequest();
@@ -61,6 +13,23 @@ request.onload = function () {
   releases = releasesAll.releves;
   console.log(releases);
 
+  // let actualHour = new Date().getHours();
+  //       // // icones dynamiques
+  //       // if (actualHour >= 6 && actualHour < 22) {
+  //       //   imgIcone.src = `styles/assets/jour/${resultatsAPI.current.weather[0].icon}.svg`;
+  //       // } else {
+  //       //   imgIcone.src = `styles/assets/nuit/${resultatsAPI.current.weather[0].icon}.svg`;
+  //       // }
+
+  //last data humidity and temperature
+  temperatureLocal.innerText = `${Math.trunc(
+    releases[releases.length - 1]["temperature"],
+  )}°`;
+  humidity.innerText = `${Math.trunc(
+    releases[releases.length - 1]["humidity"],
+  )}%`;
+
+  // Chart Data
   const xlabels = [];
   for (var i = 0; i < releases.length; i++) {
     xlabels.push(releases[i]["time_release"]);
@@ -70,18 +39,11 @@ request.onload = function () {
   for (var j = 0; j < releases.length; j++) {
     injectTemperature.push(releases[j]["temperature"]);
   }
-  temperatureLocal.innerText = `${Math.trunc(
-    releases[releases.length - 1]["temperature"],
-  )}°`;
-  humidity.innerText = `${Math.trunc(
-    releases[releases.length - 1]["humidity"],
-  )}%`;
 
   const injectHumidity = [];
   for (var k = 0; k < releases.length; k++) {
     injectHumidity.push(releases[k]["humidity"]);
   }
-  console.log("humidite", injectHumidity);
 
   const data = {
     labels: xlabels,
