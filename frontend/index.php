a7c7b149d740e7b1dc4b79dee49c297e8ec92217
sereaudrey <sere.audrey@gmail.com>
2022-03-21 08:58:52 +0100

    design interface

--- frontend/index.php
+++ frontend/index.php
@@ -3,9 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="frontend/styles/style.css">
-        <!-- Css pour la map -->
+        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="styles/popin.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+        <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet"> -->
+        <!--Lien css pour la map-->
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+        <script src="js/index.js"></script>
         <!-- Js pour la map -->
         <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
         <script src="js/map.js"></script>
@@ -13,20 +18,98 @@
         <title>Météo</title>
     </head>
     <body onload="init()">
-        <h1> Mon application météo </h1>*
-
-        <form>
-            <input placeholder="Rechercher une ville">
-        </form>
-        <div class="meteo_actuelle">
-            <span class="logo"></span>
-            <span class="ville"> Nom de la ville </span> </br>
-            <span class="temperature"> Température actuelle </span>
+        <?php
+            $dsn = 'mysql:dbname=stationmeteo;host=localhost';
+            $username = 'root';
+            $password = '';
+           
+            $con = new PDO($dsn, $username, $password);
+        ?>
+        <div class="popin-connexion" id="popinLogin">
+            <form id="formLogin" action="">
+                <div class="content-login">
+                    <div class="logo-exit">
+                        <a onclick="exitPopin()">
+                            <button id="exitPopin">
+                                <i class="fas fa-times-circle"></i>
+                            </button>
+                        </a>
+                    </div>
+                    <div class="login-mail">
+                        <span>Email : </span>
+                        <input type="text" name="mail-login" placeholder="Saisissez votre email"/>
+                    </div>
+                    <div class="login-mdp">
+                        <span>Mot de passe : </span>
+                        <input type="password" name="mdp-login" placeholder="Saisissez votre mot de passe"/>
+                    </div>
+                    <div class="login-button">
+                        <button> Créer un compte </button>
+                        <button> Connexion </button>
+                    </div>
+                </div>
+            </form>
         </div>
-
-        <div class="carte_sonde" id="map">
-            Voici où se trouve la sonde :
+        <div class="menu">
+            <div class="item logo-app">
+                <i class="fas fa-star-half-alt"></i> ATMOS
+            </div>
+            <div class="item connexion-item">
+                <a onclick="connexion()">
+                    <button id="buttonLogin">
+                        <i class="fas fa-user-circle"></i> Connexion
+                    </button> 
+                </a>
+            </div>
+            <div class="item logout-item">
+                <i class="fas fa-sign-out-alt"></i> Déconnexion
+            </div>
         </div>
-        
+        <div class="content">
+            <h1> Bienvenue sur Atmos ! </h1>
+            <i class="fa-solid fa-circle-user"></i>
+            <form class="search">
+                <input placeholder="Rechercher une ville">
+                <button class="btn-search"><i class="fas fa-search"></i></button>
+            </form>
+            
+            <div class="content-meteo-actuelle">
+                <div class="actuel position-actuelle">
+                    <div class="logo-meteo"></div>
+                    <div class="ville">
+                        <?php
+                        $ville = $con->prepare('SELECT city_sensor FROM sensors WHERE id_sensor = :1');
+                        $ville->setFetchMode(PDO::FETCH_ASSOC);
+                        $ville->execute();
+                        ?>
+                    </div> </br>
+                </div>
+                <div class="actuel temperature-actuelle">
+                    <div class="logo-meteo"></div>
+                    <div class="temperature"> °C </div> </br>
+                </div>
+                <div class=" actuel humidite-actuelle">
+                    <div class="logo-meteo"></div>
+                    <div class="humidite"> % </div> </br>
+                </div>
+            </div>
+    
+            <div class="zone-map">
+                <div class="title-map">
+                    <h2>Ma sonde</h2> <br> 
+                    <p>Villeurbanne</p>
+                </div>
+                <div class="carte_sonde" id="map"></div>
+            </div>
+            <div class="zone-releve">
+                <div class="title-releve">
+                    <h2>Historique de mes relevés</h2>
+                </div>
+                <div class="releve"></div>
+            </div>
+        </div>
+        <?php
+            $con = null;
+        ?>
     </body>
 </html>
